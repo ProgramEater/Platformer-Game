de368cec66e02e8bf1884a7038ec65f3849e9f2d
ProgramEater <stepanov.s2006@yandex.ru>
2022-12-09 22:35:23 +0300

    better movement

--- main.py
+++ main.py
@@ -6,28 +6,60 @@ class Player(pygame.sprite.Sprite):
         super().__init__(player_group)
         self.rect = pygame.rect.Rect(x, y, 30, 40)
         self.image = pygame.image.load('data/player.png')
+        self.speedX = 0
+        self.speedY = 0
+
+    def update(self):
+        self.rect.x += self.speedX
+        self.rect.y += self.speedY
+
+
+class Camera:
+    def __init__(self):
+        self.dy = 0
+
+    def update(self):
+        self.dy = height // 2 - player.rect.y - player.rect.height // 2
+
+    def apply(self, obj):
+        obj.rect = obj.rect.move(0, self.dy)
+
+
+def load_level(name):
+    with open(name) as level:
+        for y, st in enumerate(level.readlines()):
+            for x, elem in enumerate(st):
+                if elem == '#':
+                    a = pygame.sprite.Sprite(platform_group)
+                    a.image = pygame.transform.scale(pygame.image.load('data/player.png'), (20, 20))
+                    a.rect = a.image.get_rect()
+                    a.rect.x = x * 20 + 100
+                    a.rect.y = y * 20
 
 
 if __name__ == '__main__':
     pygame.init()
 
     player_group = pygame.sprite.Group()
-    player = Player(200, 200)
-    player_speed_X = 0
-    player_speed_Y = 0
+    player = Player(200, 180)
 
     platform_group = pygame.sprite.Group()
     platform1 = pygame.sprite.Sprite(platform_group)
     platform1.rect = pygame.rect.Rect(100, 300, 300, 50)
     platform1.image = pygame.transform.scale(pygame.image.load('data/player.png'), (300, 50))
+
+    load_level('data/lev.txt')
+
+    camera = Camera()
+
     # screen:
-    size = width, height = 500, 400
+    size = width, height = 1000, 800
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     PLAYER_MOVE = pygame.USEREVENT + 1
     pygame.time.set_timer(PLAYER_MOVE, 2)
     GRAVITY = pygame.USEREVENT + 2
-    pygame.time.set_timer(GRAVITY, 1)
+    pygame.time.set_timer(GRAVITY, 2)
 
     running = True
     while running:
@@ -35,38 +67,37 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                keys = pygame.key.get_pressed()
-                # a
-                if keys[97]:
-                    player_speed_X = -1
-                # s
-                if keys[100]:
-                    player_speed_X = 1
-                # both
-                if keys[100] and keys[97]:
-                    player_speed_X = 0
-                # space
-                if keys[32] and pygame.sprite.spritecollideany(player, platform_group):
-                    player.rect = player.rect.move(0, -2)
-                    player_speed_Y = -4
+                if event.key == 97:
+                    player.speedX = -1
+                if event.key == 100:
+                    player.speedX = 1
+                if event.key == 32:
+                    player.rect.y -= 1
+                    player.speedY -= 3
 
             if event.type == pygame.KEYUP:
                 keys = pygame.key.get_pressed()
-                if not keys[97] and not player_speed_Y:
-                    player_speed_X = 0
-                if not keys[100] and not player_speed_Y:
-                    player_speed_X = 0
+                if keys[97] == 0 == keys[100]:
+                    player.speedX = 0
 
             if event.type == PLAYER_MOVE:
-                player.rect = player.rect.move(player_speed_X, player_speed_Y)
+                player_group.update()
 
             if event.type == GRAVITY:
                 col = pygame.sprite.spritecollide(player, platform_group, False)
                 if col:
                     player.rect = player.rect.move(0, col[0].rect.y - player.rect.y - player.rect.height + 1)
-                    player_speed_Y = 0
+                    player.speedY = 0
                 else:
-                    player_speed_Y += 0.05
+                    player.speedY += 0.05
+
+        camera.update()
+        for i in platform_group:
+            camera.apply(i)
+        camera.apply(player)
+
+        clock.tick(60)
+
         screen.fill((255, 255, 255))
         platform_group.draw(screen)
         player_group.draw(screen)
